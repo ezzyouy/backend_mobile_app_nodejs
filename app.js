a51ef63896184b7df2ef5dbaa6f7e131d68250e8
ezzyouy <brahim.ezzyouy@gmail.com>
2021-10-25 00:15:49 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const authJwt = require("./helpers/jwt");
+
+require("dotenv/config");
+
+app.use(cors());
+app.options("*", cors());
+
+//middleware
+app.use(express.json());
+app.use(morgan("tiny"));
+app.use(authJwt());
+app.use('/public/uploads', express.static(__dirname + '/public/uploads'));
+app.use(errorHandler);
+
+//Routes
+//const categoriesRoutes = require("./routes/categories");
+const categoriesRoutes = require("./routers/categories");
+const productsRoutes = require("./routers/products");
+const usersRoutes = require("./routers/users");
+const ordersRoutes = require("./routers/orders");
+const errorHandler = require("./helpers/error-handler");
+
+const api = process.env.API_URL;
+
+app.use(`${api}/categories`, categoriesRoutes);
+app.use(`${api}/products`, productsRoutes);
+app.use(`${api}/users`, usersRoutes);
+app.use(`${api}/orders`, ordersRoutes);
+
+//Database
+mongoose
+  .connect(process.env.CONNECTION, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: "easy_shop",
+  })
+  .then(() => {
+    console.log("Database Connection is ready...");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+//Server
+app.listen(3000, () => {
+  console.log("server is running http://localhost:3000");
+});
